235abcfd733fedaffd0e0d68aae67b859d99b815
Sander.Eikenes <sander.eikenes@student.uib.no>
2024-08-15 20:34:29 +0200

    Updated theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body className={ubuntu.className} >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem
           >
           <Nav />
